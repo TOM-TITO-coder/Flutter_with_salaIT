87afb3a953b3f3a3a45f326368e822b5c68c9322
TOM-TITO <126035168+Tito-com@users.noreply.github.com>
2024-06-02 20:18:07 +0700

    Exercise 02

--- lib/main.dart
+++ lib/main.dart
@@ -36,10 +36,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          "Activities",
+          "Calculator",
           style: TextStyle(
             color: Colors.white,
             fontSize: 40,
@@ -54,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
             bottomRight: Radius.circular(10)
           )
         ),
-        backgroundColor: Colors.blue[400],
+        backgroundColor: Colors.pinkAccent,
       ),
 
       body: _buildUI(context),
@@ -94,231 +95,214 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 Widget _buildUI(BuildContext context){
+ final size = MediaQuery.of(context).size;
  return SafeArea(
-   child: Container(
-    padding: const EdgeInsets.all(20),
+   child: SizedBox(
+    //padding: const EdgeInsets.all(20),
     width: MediaQuery.of(context).size.width * 1,
     height: MediaQuery.of(context).size.height * 1,
     child: Center(
-      child: Column(
-        children: [
-          const Column(
-            children: [
-              Text(
-                "<  Yesterday  >",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                "567",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                ),
-              ),
-              Text(
-                "Calories are burned",
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 20,
-                  color: Colors.grey
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20,),
-          Row(
-            children: [
-              const SizedBox(
-                child: Icon(
-                  Icons.run_circle_outlined, 
-                  size: 100, 
-                  color: Colors.blue,
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.65,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.0),
-                      offset: const Offset(0.0, 3.0),
-                      blurRadius: 1.0,
-                    )
-                  ]
-                ),
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Steps",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "201",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "1200",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                        ),
-                        Text(
-                          " of 2000",
-                          style: TextStyle(
-                            //fontWeight: FontWeight.normal,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "You reach to more for being healty",
-                      style: TextStyle(
-                        //fontWeight: FontWeight.normal,
-                        fontSize: 15,
-                        color: Colors.grey
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20,),
-          Container(
-            padding: const EdgeInsets.all(10),
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  offset: const Offset(2, 2),
-                  blurRadius: 0.8,
-                ),
-              ]
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Trainings",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: (){}, 
-                      icon: Icon(
-                        Icons.add_circle_rounded,
-                        color: Colors.red[400],
-                        size: 40,
-                      ),
-                    )
-                  ],
-                ),
-                const Row(
-                  children: [
-                    Text("Weight Loss"),
-                    Text("256"),
-                  ],
-                ),
-                const SizedBox(height: 5,),
-                const Text(
-                  "video week2 • 45min",
+                Text(
+                  "<  Today  >",
                   style: TextStyle(
-                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
                   ),
                 ),
               ],
             ),
-          ),
-          const SizedBox(height: 30,),
-          Container(
-            padding: const EdgeInsets.all(10),
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  offset: const Offset(2, 2),
-                  blurRadius: 0.8,
-                ),
-              ]
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Exercise",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                TextButton(
+                  onPressed: (){}, 
+                  style: ButtonStyle(
+                    backgroundColor: const MaterialStatePropertyAll(Colors.pinkAccent),
+                    minimumSize: MaterialStatePropertyAll(
+                      Size(
+                        size.width * 0.45, 
+                        size.height * 0.03,
                       ),
                     ),
-                    IconButton(
-                      onPressed: (){}, 
-                      icon: Icon(
-                        Icons.add_circle_rounded,
-                        color: Colors.red[400],
-                        size: 40,
+                  ),
+                  child: const Text(
+                    "Food",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: (){}, 
+                  style: ButtonStyle(
+                    backgroundColor: const MaterialStatePropertyAll(Colors.white),
+                    minimumSize: MaterialStatePropertyAll(
+                      Size(
+                        size.width * 0.45, 
+                        size.height * 0.03,
                       ),
-                    )
-                  ],
-                ),
-                const Row(
-                  children: [
-                    Text("Running"),
-                    Text("210"),
-                  ],
-                ),
-                const SizedBox(height: 5,),
-                const Text(
-                  "15min",
-                  style: TextStyle(
-                    color: Colors.grey,
+                    ),
+                    overlayColor: const MaterialStatePropertyAll(Colors.pinkAccent)
+                  ),
+                  child: const Text(
+                    "Water",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20,
+                      color: Colors.blue
+                    ),
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  cardUI(
+                    context,
+                    title: "Dinner • 313",
+                    description: "Fried eggs",
+                    calories: "256",
+                    icon: Icons.add_circle_rounded,
+                    onButtonPressed: () {
+                      
+                    },
+                  ),
+                  const SizedBox(height: 20.0),
+                  cardUI(
+                    context,
+                    title: "Supper • 313",
+                    description: "Fried eggs",
+                    calories: "256",
+                    icon: Icons.add_circle_rounded,
+                    onButtonPressed: () {
+                      
+                    },
+                  ),
+                  const SizedBox(height: 20.0),
+                  cardUI(
+                    context,
+                    title: "Lunch • 313",
+                    description: "Fried eggs",
+                    calories: "256",
+                    icon: Icons.add_circle_rounded,
+                    onButtonPressed: () {
+                      
+                    },
+                  ),
+                  const SizedBox(height: 20.0),
+                  cardUI(
+                    context,
+                    title: "Night • 313",
+                    description: "Fried eggs",
+                    calories: "256",
+                    icon: Icons.add_circle_rounded,
+                    onButtonPressed: () {
+                      
+                    },
+                  ),
+                  //const SizedBox(height: 20.0),
+                  
+                ],
+              ),
+            )
+        
+          ],
+        ),
       ),
     ),
    ),
  );    
+}
+
+Container cardUI(BuildContext context, {
+  required String title,
+  required String description,
+  required String calories,
+  required IconData icon,
+  VoidCallback? onButtonPressed, // 
+}) {
+  return Container(
+    padding: const EdgeInsets.all(15.0),
+    width: MediaQuery.of(context).size.width * 0.9, // Adjust width for better scrolling
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10.0),
+      color: Colors.white,
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.3),
+          offset: const Offset(2.0, 2.0),
+          blurRadius: 0.8,
+        ),
+      ],
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            IconButton(
+              onPressed: onButtonPressed, // Call the provided callback if available
+              icon: Icon(
+                icon,
+                color: Colors.red[400],
+                size: 40.0,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(right: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(description),
+              Text(
+                calories,
+                style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 5.0),
+        Text(
+          "125 g",
+          style: TextStyle(color: Colors.grey),
+        ),
+        const SizedBox(height: 10.0),
+        Container(
+          margin: const EdgeInsets.only(right: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Latte"),
+              Text(
+                "57",
+                style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Text(
+          "125 g",
+          style: TextStyle(color: Colors.grey),
+        ),
+      ],
+    ),
+  );
 }
